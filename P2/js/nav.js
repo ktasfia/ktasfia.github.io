381b765e7878fd2544825ffd2f20f90186c01a0a
ktasfia <57448200+ktasfia@users.noreply.github.com>
2021-11-20 03:46:43 -0500

    Update nav.js

--- P2/js/nav.js
+++ P2/js/nav.js
@@ -50,3 +50,19 @@ window.onscroll = () => {
   if(this.scrollY <= 13) nav.className = ''; else nav.className = 'scroll';
 };
 
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+
+var currentScrollpos = window.pageYOffset;
+if(prevScrollpos > currentScrollpos) {
+      document.getElementById("navbar").style.top = "0";
+} else {
+      document.getElementById("navbar").style.top = "-100px";
+}
+
+prevScrollpos = currentScrollpos;
+
+}
+
